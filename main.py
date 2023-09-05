d4125f1ac1f1c04dddc0412265acef2072e053ed
shreyassarkar31 <residentclancyjavis@gmail.com>
2023-09-05 21:52:40 +0530

    updated

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
 from sklearn.metrics import accuracy_score
 
-dataset = p.read_csv('KNN Dataset.csv')
+dataset = p.read_csv('KNN_Dataset.csv')
 
 print(len(dataset))
+print(dataset.head())
